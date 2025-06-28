0586577458bb3efbc500279d43a6e85ca19274aa
Ulisses Dias <ulisses@diasify.com>
2025-06-29 11:24:34 +1200

    Add descriptions to sources and tables in sources.yml

--- jaffle_shop/models/marts/dim_customers.sql
+++ jaffle_shop/models/marts/dim_customers.sql
@@ -6,6 +6,6 @@ select
     max(order_date) as last_order_date,
     count(order_id) as total_orders,
     sum(order_total) as total_spent
-from {{ ref('stg_orders') }}
-left join {{ ref('stg_customers') }} using (customer_id)
+from {{ ref('stg_customers') }}
+left join {{ ref('stg_orders') }} using (customer_id)
 group by customer_id, first_name, last_name
