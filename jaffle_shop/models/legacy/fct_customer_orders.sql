e45a8f074a45038878512f44314f8d993dbcfb54
Ulisses Dias <ulisses@diasify.com>
2025-07-05 22:26:31 +1200

    Fix payment source reference and refactor CTE for clarity and modularity in customer orders model.

--- jaffle_shop/models/legacy/fct_customer_orders.sql
+++ jaffle_shop/models/legacy/fct_customer_orders.sql
@@ -3,79 +3,79 @@ with
 -- Import CTEs
 
 customers as (
-
   select * from {{ source('jaffle_shop', 'customers') }}
-
 ),
 
 orders as (
-
   select * from {{ source('jaffle_shop', 'orders') }}
-
 ),
 
 payments as (
-
-  select * from {{ source('stripe', 'payment') }}
-
+  select * from {{ source('stripe', 'payments') }}
 ),
 
--- Logical CTEs
--- Final CTE
--- Simple Select Statment
-
+-- Logical CTE
+order_amount as (
+    select 
+        orderid as order_id,
+        max(created) as payment_finalized_date,
+        sum(amount) as total_amount_paid
+    from payments
+    where status <> 'fail'
+    group by 1
+),
 
 paid_orders as (
     select orders.id as order_id,
         orders.user_id as customer_id,
         orders.order_date as order_placed_at,
         orders.status as order_status,
-        p.total_amount_paid,
-        p.payment_finalized_date,
-        c.first_name as customer_first_name,
-        c.last_name as customer_last_name
+        order_amount.total_amount_paid,
+        order_amount.payment_finalized_date,
+        customers.first_name as customer_first_name,
+        customers.last_name as customer_last_name
     from orders
-    left join (
-        select 
-            orderid as order_id,
-            max(created) as payment_finalized_date,
-            sum(amount) / 100.0 as total_amount_paid
-        from payments
-        where status <> 'fail'
-        group by 1
-    ) p on orders.id = p.order_id
-    left join customers as c on orders.user_id = c.id ),
+    left join order_amount on orders.id = order_amount.order_id
+    left join customers on orders.user_id = customers.id
+),
 
-customer_orders as (
-    select 
-        c.id as customer_id
-        , min(order_date) as first_order_date
-        , max(order_date) as most_recent_order_date
-        , count(orders.id) as number_of_orders
-    from customers as c 
-    left join orders on orders.user_id = c.id 
-    group by 1
+ltv as (
+    select
+        paid_orders.order_id,
+        sum(total_amount_paid) over (
+            partition by paid_orders.customer_id
+            order by paid_orders.order_id
+            rows between unbounded preceding and current row
+        ) as clv
+    from paid_orders
+    order by paid_orders.order_id
+),
+
+-- Final CTE
+final as (
+    select
+        paid_orders.*,
+        row_number() over (order by paid_orders.order_id) as transaction_seq,
+        row_number() over (partition by customer_id order by paid_orders.order_id) as customer_sales_seq,
+        case
+            when (
+                rank() over (
+                    partition by customer_id
+                    order by paid_orders.order_placed_at, paid_orders.order_id
+                ) = 1
+            ) then 'new'
+            else 'return' end as nsvr,
+        ltv.clv as customer_lifetime_value,
+        first_value(paid_orders.order_placed_at) over (
+            partition by paid_orders.customer_id
+            order by paid_orders.order_id
+        ) as fdos
+    from paid_orders
+    left outer join ltv on ltv.order_id = paid_orders.order_id
+    order by order_id
 )
 
-select
-    p.*,
-    row_number() over (order by p.order_id) as transaction_seq,
-    row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
-    case when c.first_order_date = p.order_placed_at
-    then 'new'
-    else 'return' end as nvsr,
-    x.clv_bad as customer_lifetime_value,
-    c.first_order_date as fdos
-from paid_orders p
-left join customer_orders as c using (customer_id)
-left outer join 
-(
-    select
-        p.order_id,
-        sum(t2.total_amount_paid) as clv_bad
-    from paid_orders p
-    left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
-    group by 1
-    order by p.order_id
-) x on x.order_id = p.order_id
+-- Simple Select Statment
+select * from final
 order by order_id
+
