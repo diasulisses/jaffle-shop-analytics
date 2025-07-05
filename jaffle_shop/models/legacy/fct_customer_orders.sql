b17e6ab29a279c4412f75eb4f46aa5bf7efc1587
Ulisses Dias <ulisses@diasify.com>
2025-07-05 23:21:46 +1200

    Fix typo in alias for new vs returning customer and update cumulative lifetime value alias for clarity

--- jaffle_shop/models/legacy/fct_customer_orders.sql
+++ jaffle_shop/models/legacy/fct_customer_orders.sql
@@ -41,13 +41,13 @@ final as (
                     order by paid_orders.order_placed_at, paid_orders.order_id
                 ) = 1
             ) then 'new'
-            else 'return' end as nsvr,
+            else 'return' end as nvsr,
 
         -- customer lifetime value
         sum(total_amount_paid) over (
             partition by paid_orders.customer_id
             order by paid_orders.order_placed_at, paid_orders.order_id
-        ) as cumulative_lifetime_value,
+        ) as customer_lifetime_value,
 
         -- first order date
         first_value(paid_orders.order_placed_at) over (
